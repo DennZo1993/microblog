fcbadeec3d0accc6a3a0a9361bc49be92bb1d537
Denis Zobnin <d.zobnin.work@gmail.com>
2018-05-13 21:02:34 +0300

    Switch to application factory

--- app/__init__.py
+++ app/__init__.py
@@ -10,35 +10,43 @@ from flask_bootstrap import Bootstrap
 from config import Config
 
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
 
-from app.errors import errors_bp
-app.register_blueprint(errors_bp)
+def create_app(config_obj=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
 
-from app.auth import auth_bp
-app.register_blueprint(auth_bp)
+    # Register blueprints
+    from app.errors import errors_bp
+    app.register_blueprint(errors_bp)
 
-from app.main import main_bp
-app.register_blueprint(main_bp)
+    from app.auth import auth_bp
+    app.register_blueprint(auth_bp)
 
-app.config.from_object(Config)
+    from app.main import main_bp
+    app.register_blueprint(main_bp)
 
-if not app.debug:
-    if app.config['LOG_TO_STDERR']:
-        log_handler = StreamHandler()
-    else:
-        log_dir = app.config['LOG_DIRECTORY']
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
-        log_file = os.path.join(log_dir, app.config['LOG_MAIN_FILE'])
-        log_handler = RotatingFileHandler(filename=log_file, maxBytes=10240, backupCount=10)
+    # Init application
+    bootstrap.init_app(app)
 
-    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    log_handler.setFormatter(formatter)
-    log_handler.setLevel(logging.INFO)
+    # Init logging
+    if not app.debug:
+        if app.config['LOG_TO_STDERR']:
+            log_handler = StreamHandler()
+        else:
+            log_dir = app.config['LOG_DIRECTORY']
+            if not os.path.exists(log_dir):
+                os.mkdir(log_dir)
+            log_file = os.path.join(log_dir, app.config['LOG_MAIN_FILE'])
+            log_handler = RotatingFileHandler(filename=log_file, maxBytes=10240, backupCount=10)
 
-    app.logger.removeHandler(default_handler)
-    app.logger.addHandler(log_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        log_handler.setFormatter(formatter)
+        log_handler.setLevel(logging.INFO)
+
+        app.logger.removeHandler(default_handler)
+        app.logger.addHandler(log_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+    return app
